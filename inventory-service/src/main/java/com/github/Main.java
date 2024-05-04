c53479c3d8859202e46355c28f8090941e3f7ef2
pedroppd <pedro_pdantas@hotmail.com>
2024-05-04 16:17:03 -0300

    feat: adding application's port

--- inventory-service/src/main/java/com/github/Main.java
+++ inventory-service/src/main/java/com/github/Main.java
@@ -1,7 +1,7 @@
 package com.github;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
     }
 }
