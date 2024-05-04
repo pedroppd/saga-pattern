81613465dd384fdef8fa4db141ea7164d583ca1e
pedroppd <pedro_pdantas@hotmail.com>
2024-05-04 16:02:06 -0300

    feat: Creating modules

---
+++ src/main/java/com/github/sagapattern/SagaPatternApplication.java
@@ -0,0 +1,13 @@
+package com.github.sagapattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaPatternApplication.class, args);
+	}
+
+}
